448b496eb51a013f7ee3e90fcf0ee5c583973966
kleshninpo <kleshnin.p.o@gmail.com>
2024-01-05 19:51:22 +0400

    feat(api): added handleFetchErrors function

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,12 @@
 import { IAuthor, IMessage } from './types';
 
+const handleFetchErrors = (response: Response) => {
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+  return response.json();
+};
+
 // TODO: здесь ещё, по-хорошему, надо бы добавлять в authors нового автора с id и датой рождения
 export const addMessage = (message: IMessage) => {
   return fetch('http://localhost:3001/messages', {
@@ -8,16 +15,16 @@ export const addMessage = (message: IMessage) => {
       'Content-Type': 'application/json;charset=utf-8',
     },
     body: JSON.stringify({ ...message }),
-  }).catch((e) => {
-    console.error('Error adding new message:', e);
-  });
+  })
+    .then(handleFetchErrors)
+    .catch((e) => {
+      console.error('Error adding new message:', e);
+    });
 };
 
 export const getMessages = (): Promise<IMessage[]> => {
   return fetch('http://localhost:3001/messages')
-    .then((messages) => {
-      if (messages.status === 200) return messages.json();
-    })
+    .then(handleFetchErrors)
     .catch((e) => {
       console.error('Error fetching messages:', e);
     });
@@ -25,10 +32,16 @@ export const getMessages = (): Promise<IMessage[]> => {
 
 export const getAuthor = (author: string): Promise<IAuthor[]> => {
   return fetch(`http://localhost:3001/authors?fullName=${author}`)
-    .then((authorData) => {
-      if (authorData.status === 200) return authorData.json();
-    })
+    .then(handleFetchErrors)
     .catch((e) => {
       console.error('Error fetching author:', e);
+    });
+};
+
+export const getMessagesByAuthor = (author: string): Promise<IMessage[]> => {
+  return fetch(`http://localhost:3001/messages?author=${author}`)
+    .then(handleFetchErrors)
+    .catch((e) => {
+      console.error(`Error fetching author's messages: `, e);
     });
 };
